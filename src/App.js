91ac97b24f70cb618eb79427e3cfa535edbb6050
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-04-03 14:56:45 -0700

    started project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [results, setResults] = useState();
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+      console.log(response.data.name);
+      setCountries(response.data.name);
+    });
+  }, []);
+
+  const handleCountries = event => {
+    setCountries(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        {" "}
+        Find Countries: <input value={countries} onChange={handleCountries} />
+      </p>
+      <div>{countries}</div>
     </div>
   );
 }
